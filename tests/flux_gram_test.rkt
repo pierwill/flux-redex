d9638b033959b8fa998da796d99762a98bb44374
pierwill <pierwill@users.noreply.github.com>
2022-09-11 14:27:41 -0500

    Add basic tests for reduction relations
    Create tests folder

--- tests/flux_gram_test.rkt
+++ tests/flux_gram_test.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require redex
-         "flux.rkt"
+         "../flux.rkt"
          )
 
 (module+ test
@@ -203,37 +203,5 @@
   ;;     add(a: {}, b: {})
   ;; See spec example.
 
-  ;;
-  (test-->>
-   flux-red
-   (term (3 "^" 2))
-   (term 9))
-
-  (test-->>
-   flux-red
-   (term (3 "*" 2))
-   (term 6))
-
-  (test-->>
-   flux-red
-   (term (true "and" false))
-   (term false))
-
-  (test-->>
-   flux-red
-   (term (true "or" false))
-   (term true))
-
-  ;; (test-->>
-  ;; flux-red
-  ;; (term (true "==" false))
-  ;; (term false))
-
-  ;; (apply-reduction-relation flux-red (term (9 "/" 4)))
-  ;; (apply-reduction-relation flux-red (term (9 "==" 9)))
-  ;; (apply-reduction-relation flux-red (term (sup "!=" 9)))
-  ;; (apply-reduction-relation flux-red (term (9 ">=" 4)))
-  ;; (apply-reduction-relation flux-red (term (4 "^" 2)))
-
-  ;; end test module
+  ;; 
   )
---
+++ tests/flux_sem_test.rkt
@@ -0,0 +1,48 @@
+#lang racket
+(require redex
+         "../flux.rkt"
+         )
+
+(module+ test
+
+  ;; SEMANTICS
+  ;; =========
+  
+  (test-->>
+   flux-red
+   (term (3 "^" 2))
+   (term 9))
+
+  (test-->>
+   flux-red
+   (term (3 "*" 2))
+   (term 6))
+
+  ;; (test-->>
+  ;;  flux-red
+  ;;  (term (4 "^" (2 "*" 2)))
+  ;;  (term 256))
+  
+  (test-->>
+   flux-red
+   (term (true "and" false))
+   (term false))
+
+  (test-->>
+   flux-red
+   (term (true "or" false))
+   (term true))
+
+  ;; (test-->>
+  ;; flux-red
+  ;; (term (true "==" false))
+  ;; (term false))
+
+  ;; (apply-reduction-relation flux-red (term (9 "/" 4)))
+  ;; (apply-reduction-relation flux-red (term (9 "==" 9)))
+  ;; (apply-reduction-relation flux-red (term (sup "!=" 9)))
+  ;; (apply-reduction-relation flux-red (term (9 ">=" 4)))
+  ;; (apply-reduction-relation flux-red (term (4 "^" 2)))
+
+  ;; end test module
+  )
