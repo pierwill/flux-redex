d6b1ea1a072ca38295613282956e073082695d3d
pierwill <pierwill@users.noreply.github.com>
2022-09-15 12:24:59 -0500

    Add in-hole for and, not, or

--- src/reduction.rkt
+++ src/reduction.rkt
@@ -7,26 +7,42 @@
 
   (Val ::= IntVal
        StringVal
+       BoolVal
        "null"
        ;; temporary hack
-       ;; TODO use BoolVal ::= true false and define them in a Store
-       #f #t
+       ;; define these in a Store
+       ;; (term #t)
+       ;; (term #f)
        )
 
   (IntVal ::= IntLit)
   (StringVal ::= StringLit)
+  (BoolVal ::= #f #t)
 
   (E ::= hole
      (E "+" Expression)
      (Val "+" E)
+
      (E "-" Expression)
      (Val "-" E)
+
      (E "*" Expression)
      (Val "*" E)
+
      (E "/" Expression)
      (Val "/" E)
+
      (E "==" Expression)
      (Val "==" E)
+
+     (E "and" Expression)
+     (Val "and" E)
+
+     (E "or" Expression)
+     (Val "or" E)
+
+     ("not" E)
+
      ("exists" E)
      )
 
@@ -44,19 +60,16 @@
         ,(if (term Expression_1) (term Expression_2) (term Expression_3))
         "if-then-else")
 
-   ;; TODO
-   (--> (Expression_1 "and" Expression_2)
-        ,(and (term Expression_1) (term Expression_2))
+   (--> (in-hole E (Val_1 "and" Val_2))
+        (in-hole E ,(and (term Val_1) (term Val_2)))
         "and")
 
-   ;; TODO
-   (--> (Expression_1 "or" Expression_2)
-        ,(or (term Expression_1) (term Expression_2))
+   (--> (in-hole E (Val_1 "or" Val_2))
+        (in-hole E ,(or (term Val_1) (term Val_2)))
         "or")
 
-   ;; TODO
-   (--> ("not" Expression_1)
-        ,(not (term Expression_1))
+   (--> (in-hole E ("not" Val_1))
+        (in-hole E ,(not (term Val_1)))
         "not")
 
    (--> (in-hole E ("exists" Val))
@@ -92,7 +105,7 @@
         "greater than or eq")
 
    (--> (in-hole E (integer_1 "+" integer_2))
-        (in-hole E  ,(+ (term integer_1) (term integer_2)))
+        (in-hole E ,(+ (term integer_1) (term integer_2)))
         "add")
 
    ;; (--> (in-hole E (IntLit_1 "+" IntLit_2))
@@ -142,30 +155,43 @@
   (test-->> flux-red (term (1 "-" 1)) (term 0))
   (test-->> flux-red (term (9 "-" (4 "-" 2))) (term 7))
   (test-->> flux-red (term (1 "+" (4 "+" 2))) (term 7))
+  (test-->> flux-red (term ((3 "+" 2) "+" (3 "+" 2))) (term 10)) ; 🖜 () + () works here
   (test-->> flux-red (term (3 "^" 2)) (term 9))
   (test-->> flux-red (term (3 "*" 2)) (term 6))
   (test-->> flux-red (term (3 "*" (3 "*" 4))) (term 36))
   (test-->> flux-red (term (12 "/" 4)) (term 3))
   (test-->> flux-red (term (12 "/" (2 "+" 2))) (term 3))
+  (test-->> flux-red (term (6 "==" (4 "+" 2))) (term #t))
+  (test-->> flux-red (term ((4 "+" 2) "==" 7)) (term #f))
   (test-->> flux-red (term ("exists" "null")) (term #f))
   (test-->> flux-red (term ("exists" "hello")) (term #t))
   (test-->> flux-red (term ("exists" ("exists" "hello"))) (term #t))
   (test-->> flux-red (term ("exists" ("exists" "null"))) (term #t))
+  (test-->> flux-red (term (#t "and" #f)) (term #f))
+  (test-->> flux-red (term (#t "and" #t)) (term #t))
+  (test-->> flux-red (term (#t "and" (#t "and" #f))) (term #f))
+  (test-->> flux-red (term (#t "or" #f)) (term #t))
+  (test-->> flux-red (term (#t "or" #t)) (term #t))
+  (test-->> flux-red (term (#f "or" (#t "or" #f))) (term #t))
+  (test-->> flux-red (term ("not" #f)) (term #t))
+  (test-->> flux-red (term ("not" ("not" #f))) (term #f))
+  (test-->> flux-red (term ("not" ("not" ("not" ("not" #f))))) (term #f))
+
+  ;; FIXME () + ()
+  ;; (test-->> flux-red (term ((#t "and" #f) "and" (#t "and" #f))) (term #f))
+  ;; (test-->> flux-red (term ((#t "or" #f) "or" (#t "or" #f))) (term #f))
+
+  ;; de Morgan's laws
+  ;; (test-->> flux-red
+  ;;           (term ("not" (#t "or" #t)))
+  ;;           (term (("not" #t) "or" ("not" #t)))
+  ;;           ))
+
   ;; (test-->> flux-red (term ("if" true "then" false "else" true)) (term false))
-  ;; (test-->> flux-red (term (true "and" false)) (term false))
   ;; (test-->> flux-red (term (true "or" false)) (term true))
-
-  ;; TODO
   ;; (test-->> flux-red (term (1 ">" (2 "^" 2))) (term false))
-
-  ;; FIXME #f vs false
   ;; (test-->> flux-red (term (32 "!=" 31)) (term true))
-
-  ;; FIXME #f vs false
   ;; (test-->> flux-red (term (true "==" false)) (term false))
 
   ;;
   )
-
-;; (traces flux-red (term (6 "==" (4 "+" 2))))
-;; (traces flux-red (term ((4 "+" 2) "==" 7)))
